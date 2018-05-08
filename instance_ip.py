3bafd943efb74f71a840af635aa405935a12a5a8
Mahendra Mamidi <mahendram1906@gmail.com>
2018-05-08 12:51:26 +0000

    modified instance_ip.py

--- instance_ip.py
+++ instance_ip.py
@@ -3,13 +3,16 @@ ec2=boto3.resource("ec2")
 print "Name\t\tStatus\t\tInstance_ID\t\t\tPublic_IP\t\tPrivate_IP"
 print "----\t\t------\t\t-----------\t\t\t---------\t\t----------"
 for i in ec2.instances.all():
-    #name=i.tags[0]['Value']
     pip=i.public_ip_address
-    if (i.tags[0]['Value']) == None:
-        print "","\t\t",i.state['Name'],"\t", i.id,"\t\t", i.public_ip_address,"\t\t\t", i.private_ip_address
-    elif i.state['Name']=='terminated':
-        print "","\t\t",i.state['Name'],"\t", i.id,"\t\t", i.public_ip_address,"\t\t\t", i.private_ip_address
-    elif pip  != 'None' and len(i.tags[0]['Value'])<=6:
-        print i.tags[0]['Value'],"\t\t",i.state['Name'],"\t", i.id,"\t\t", i.public_ip_address,"\t\t", i.private_ip_address
+    if (i.private_ip_address == None):
+        print i.tags[0]['Value'],"\t\t",i.state['Name'],"\t",i.id,"\t\t",i.public_ip_address,"\t\t\t",i.private_ip_address
+
+    #elif i.state['Name']=='terminated':
+    #    print "","\t\t",i.state['Name'],"\t", i.id,"\t\t", i.public_ip_address,"\t\t\t", i.private_ip_address
+
+    elif pip != None and len(i.tags[0]['Value'])<=6:
+        print i.tags[0]['Value'],"\t\t",i.state['Name'],"\t",i.id,"\t\t",i.public_ip_address,"\t\t",i.private_ip_address
+    elif pip == None and len(i.tags[0]['Value'])>6:
+        print i.tags[0]['Value'],"\t",i.state['Name'],"\t",i.id,"\t\t",i.public_ip_address,"\t\t\t",i.private_ip_address
     else:
-        print i.tags[0]['Value'],"\t",i.state['Name'],"\t", i.id,"\t\t", i.public_ip_address,"\t\t\t", i.private_ip_address
+        print i.tags[0]['Value'],"\t",i.state['Name'],"\t",i.id,"\t\t",i.public_ip_address,"\t\t",i.private_ip_address
